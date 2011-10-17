d43aa44eefe81721dffbc0bf72015fe5b28e2a2c
Łukasz Mazurek <lpmazurek@gmail.com>
2011-10-17 20:46:40 +0200

    First commit with bad compression

--- sad_spe.c
+++ sad_spe.c
@@ -4,7 +4,8 @@
 #include "spe.h"
 #include "spe_only.h"
 
-#define REF_WIDTH 39
+#define REF_WIDTH 48
+#define VEC_REF_WIDTH REF_WIDTH / 16
 #define REF_HEIGHT 39
 #define ORIG_WIDTH 8
 #define ORIG_HEIGHT 8
@@ -25,12 +26,11 @@ uint8_t ref_array[REF_WIDTH * REF_HEIGHT] __attribute__((aligned(128)));
 uint8_t read_tmp_array[256] __attribute__((aligned(128)));
 int sad[SAD_WIDTH * SAD_HEIGHT] __attribute__((aligned(128)));
 
-sad_params_t params __attribute__((aligned(128)));
 sad_out_t sad_out __attribute__((aligned(128)));
 
-uint8_t *orig;
-uint8_t *ref;
-uint8_t *read_tmp;
+VUC *orig;
+VUC *ref;
+VUC *read_tmp;
 
 unsigned mbox_data[4];
 int w, wm128, orig_offset, ref_offset, orig_x, orig_y, ref_w, ref_h, sad_w, sad_h;
@@ -39,41 +39,31 @@ VUC reg;
 VUC tmp;
 VUC sd;
 
-void read_row(uint8_t *dst, ULL src, int size, int offset) {
-    int i, tag = 1;
+void read_row(VUC *dst, ULL src, int size, int offset) {
+    int tag = 1;
     int read_size = size + offset;
     read_size += (16 - (read_size%16)) % 16;
     spu_mfcdma64(read_tmp, mfc_ea2h(src), mfc_ea2l(src), 
             (read_size) * sizeof(uint8_t), tag, MFC_GET_CMD);
     spu_writech(MFC_WrTagMask, 1 << tag);
     spu_mfcstat(MFC_TAG_UPDATE_ALL);
-    for (i = 0; i < size; ++i)
-        dst[i] = read_tmp[offset + i];
 }
 
+// stores into "reg" two rows of ref beginnig from x,y
+// assumption : REF_WIDTH % 16 = 0
 void get_ref(int x, int y) {
-    int offset1 = (y*ref_w + x) % 16;
-    int offset2 = (offset1 + ref_w) % 16;
-    uint8_t *ref_ptr = &ref[y*ref_w + x - offset1];
+    int offset = (y*REF_WIDTH + x) % 16;
+    int block = y*VEC_REF_WIDTH + x/16;
 
-    VUC *ref_ptr1 = (VUC *) ref_ptr;
-    VUC *ref_ptr2 = (VUC *) (ref_ptr + 16);
+    reg = spu_shuffle(ref[block], ref[block + 1],  mask[offset]);
 
-    reg = spu_shuffle(*ref_ptr1, *ref_ptr2,  mask[offset1]);
+    block += VEC_REF_WIDTH;
 
-    ref_ptr = &ref[(y+1)*ref_w + x - offset2];
-    ref_ptr1 = (VUC *) ref_ptr;
-    ref_ptr2 = (VUC *) (ref_ptr + 16);
-
-    tmp = spu_shuffle(*ref_ptr1, *ref_ptr2, mask[offset2]);
-
+    tmp = spu_shuffle(ref[block], ref[block + 1], mask[offset]);
     reg = spu_shuffle(reg, tmp, merge_mask);
 }
 
-int sad16(uint8_t *orig_reg_scalar) {
-    VUC orig_reg = 
-        *((VUC *) orig_reg_scalar);
-
+int sad16(VUC orig_reg) {
     sd = spu_absd(orig_reg, reg);
     uint8_t *s = (uint8_t *) &sd;
 
@@ -86,7 +76,7 @@ int calc_sad(int x, int y) {
     int sum = 0;
     for (i = 0; i < 4; i++) {
         get_ref(x, y);
-        sum += sad16(orig + 16*i);
+        sum += sad16(orig[i]);
         y += 2;
     }
     return sum; 
@@ -146,11 +136,14 @@ void ds(int x, int y) {
 }
 
 int main(ULL spe, ULL argp, ULL envp) {
-    int tag = 1;
+    sad_params_t params __attribute__((aligned(128)));
+    int tag = 1, i, j;
+
     while(1) {
-        orig = orig_array;
-        ref = ref_array;
-        read_tmp = read_tmp_array;
+        orig = (VUC *) orig_array;
+        ref = (VUC *) ref_array;
+        read_tmp = (VUC *) read_tmp_array;
+
         mbox_data[0] = spu_read_in_mbox();
         mbox_data[1] = spu_read_in_mbox();
         mbox_data[2] = spu_read_in_mbox();
@@ -177,27 +170,59 @@ int main(ULL spe, ULL argp, ULL envp) {
         sad_w = ref_w - 7;
         sad_h = ref_h - 7;
 
-        // GET orig
-        int i;
-        for (i = 0; i < ORIG_HEIGHT; ++i) {
-            read_row(orig, params.orig, 8, orig_offset);
-            orig += ORIG_WIDTH;
+        // GET orig (two rows in each loop step)
+        for (i = 0; i < ORIG_HEIGHT / 2; ++i) {
+            // read 8 bytes and everything on the left to 128 boundary
+            read_row(read_tmp, params.orig, 8, orig_offset);
+            // shift and copy to orig[i]
+            orig[i] = spu_shuffle(read_tmp[orig_offset / 8],
+                    read_tmp[orig_offset / 8 + 1], mask[orig_offset % 8]);
+            // jump to next row in input
+            params.orig += w + orig_offset;
+            orig_offset = params.orig % 128;
+            params.orig -= orig_offset;
+
+            // read 8 bytes
+            read_row(read_tmp, params.orig, 8, orig_offset);
+            // shift
+            read_tmp[0] = spu_shuffle(read_tmp[orig_offset / 8],
+                    read_tmp[orig_offset / 8 + 1], mask[orig_offset % 8]);
+            // merge
+            orig[i] = spu_shuffle(orig[i], read_tmp[0], merge_mask);
+            // jump to next row in input
             params.orig += w + orig_offset;
             orig_offset = params.orig % 128;
             params.orig -= orig_offset;
         }
         
         // GET ref
+        int vectors = ref_w / 16 + (ref_w % 16 > 0);
         for (i = 0; i < ref_h; ++i) {
-            read_row(ref, params.ref, ref_w, ref_offset);
-            ref += ref_w;
+            // read row of input (and shit on the left)
+            read_row(read_tmp, params.ref, ref_w, ref_offset);
+            // shift block by block
+            for (j = 0; j < vectors; ++j) {
+                // first block (8) in vector (16)
+                ref[i * VEC_REF_WIDTH + j] = spu_shuffle(
+                        read_tmp[ref_offset / 8 + j],
+                        read_tmp[ref_offset / 8 + j + 1],
+                        mask[ref_offset % 8]);
+                // second block (8) in vector (16)
+                read_tmp[0] = spu_shuffle(
+                        read_tmp[ref_offset / 8 + j + 1],
+                        read_tmp[ref_offset / 8 + j + 2],
+                        mask[ref_offset % 8]);
+                // merge
+                ref[i * VEC_REF_WIDTH + j] = spu_shuffle(
+                        ref[i * VEC_REF_WIDTH + j],
+                        read_tmp[0],
+                        merge_mask);
+            }
+            // jump to next row in input
             params.ref += w + ref_offset;
             ref_offset = params.ref % 128;
             params.ref -= ref_offset;
         }
-        ref = ref_array;
-        orig = orig_array;
-        read_tmp = read_tmp_array;
 
         // calc
         ds_init();
